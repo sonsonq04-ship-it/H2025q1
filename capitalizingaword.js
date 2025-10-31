c0a3d9d3f2da50cb55830cd902d52bab2a47af6c
sonya <sonsonq04@gmail.com>
2025-10-31 13:06:11 +0300

    rabota za 31 oktyabrya

---
+++ capitalizingaword.js
@@ -0,0 +1,7 @@
+const sobachka = 'milaia'
+const firstlettar = sobachka.slice( 0, 1).toUpperCase()
+console.log(firstlettar)
+const twolettat = sobachka.slice(1, sobachka.length)
+console.log(twolettat)
+const result=firstlettar+twolettat
+console.log(result)
